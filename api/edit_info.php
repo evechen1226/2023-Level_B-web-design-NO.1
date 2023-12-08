1b191da81dbe1f6334e56c45566739d7c2eb2d78
evechen1226 <eveliving2021@gmail.com>
2023-12-08 11:33:14 +0800

    feat:增加modal/title.php

--- api/edit_info.php
+++ api/edit_info.php
@@ -1,16 +1,21 @@
 <?php include_once "db.php";
 
 //因為要共用，所以需要增加一input隱藏欄位存入當前網頁名稱
+
 //取得資料表名稱
 $table=$_POST['table'];
 
+//將資料表名稱轉成首字大寫的資料物件變數
 $DB=${ucfirst($table)};
+//$DB=$Table
 
+//取得id為1的資料
 $data-$DB->find(1);
 
+//將資料中對應的欄位修改為POST過來值
 $data['$table']=$_POST['$table'];
 
-
+//使用save更新至資料表
 $DB->save($date);
 
 
