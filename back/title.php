d0177afdfa95df9e3d39f9ea4cab9fa170e0714e
evechen1226 <eveliving2021@gmail.com>
2023-12-08 14:31:25 +0800

    feat:title.php-新增列表功能(foreach)

--- back/title.php
+++ back/title.php
@@ -1,27 +1,42 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-					<p class="t cent botli">網站標題管理</p>
-					<form method="post" target="back" action="?do=tii">
-						<table width="100%">
-							<tbody>
-								<tr class="yel">
-									<td width="45%">網站標題</td>
-									<td width="23%">替代文字</td>
-									<td width="7%">顯示</td>
-									<td width="7%">刪除</td>
-									<td></td>
-								</tr>
-							</tbody>
-						</table>
-						<table style="margin-top:40px; width:70%;">
-							<tbody>
-								<tr><img src="../" alt="">
-								<input type="hidden" name="table" value="<?=$do;?>">
-									<td width="200px"><input type="button" onclick="op('#cover','#cvr','../modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增網站標題圖片"></td>
-									<td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
-									</td>
-								</tr>
-							</tbody>
-						</table>
+	<p class="t cent botli">網站標題管理</p>
+	<!-- <form method="post" target="back" action="?do=tii"> -->
+	<form method="post" action="?do=tii">
+		<table width="100%">
+			<tbody>
+				<tr class="yel">
+					<td width="45%">網站標題</td>
+					<td width="23%">替代文字</td>
+					<td width="7%">顯示</td>
+					<td width="7%">刪除</td>
+					<td></td>
+				</tr>
+				<?php $rows = $Title->all();
+				foreach ($rows as $row) {
+				?>
+                    <td width="45%">
+						<img src="./img/<?= $row['img'];?>" alt="" style="width:300px;height:30px">
+					</td>
+                    <td width="23%"><input type="text" name="text[<?= $row['id'];?>]" value="<?= $row['text'];?>" style="width:90%"></td>
+                    <td width="7%"><input type="radio" name="sh" value="<?= $row['id'];?>"
+					 id=""></td>
+                    <td width="7%"><input type="checkbox" name="del[]" value="<?= $row['id'];?>"></td>
+                    <td>
+						<input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?= $id;?>&id=<?= $row['id'];?>')" value="更新圖片">
+					</td>
 
-					</form>
-				</div>
+				<?php
+				} ?>
+			</tbody>
+		</table>
+        <table style="margin-top:40px; width:70%;">
+            <tbody>
+                <tr>
+                    <input type="hidden" name="table" value="<?= $do; ?>">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增網站標題圖片"></td>
+                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+                </tr>
+            </tbody>
+        </table>
+	</form>
+</div>
