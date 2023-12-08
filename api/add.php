8c1f3bda16539c2b23e2993f2137a8d647d510fa
evechen1226 <eveliving2021@gmail.com>
2023-12-08 14:10:38 +0800

    fix:title.php-刪除form中的traget，更改modal連結

--- api/add.php
+++ api/add.php
@@ -1,3 +1,20 @@
 <?php 
 include_once "db.php";
+
+$DB=${ucfirst($_POST['table'])};
+//假設為 do=?img
+//$DB = Img
+$table=$_POST['table'];
+//$table = img
+
+if(isset($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../img".$_FILES['img']['name']);
+    $_POST['img']=$_FILES['img']['name'];
+}
+
+unset($_POST['table']);
+$DB->save($_POST);
+
+to("../back.php?do=$table");
+
 ?>
