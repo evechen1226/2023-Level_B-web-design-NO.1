8f5741aca8a1fc6deff8375219be05d845af30e0
evechen1226 <eveliving2021@gmail.com>
2023-12-11 13:30:30 +0800

    refactor:將?do=imges改為$do

--- back/image.php
+++ back/image.php
@@ -52,21 +52,21 @@
 		<div class="cent">
 			<?php
 			if($now>=1){
-				// echo "<a href='?do=image&p='".($now-1)."</a>";
+				// echo "<a href='?do=$do&p='".($now-1)."</a>";
 				$prev=$now-1;
-				echo "<a href='?do=image&p=$prev'> < </a>";
+				echo "<a href='?do=$do&p=$prev'> < </a>";
 			}
 			for ($i=1;$i<=$pages;$i++){
 				
 				$fontsize=($now==$i)?'24px':'16px';
 				
-				echo "<a href='?do=image&p=$i' style='font-size:$fontsize'>$i</a>";
+				echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'>$i</a>";
 			}
 
 			if($now<$pages){
 
 				$next=$now+1;
-				echo "<a href='?do=image&p=$next'> > </a>";
+				echo "<a href='?do=$do&p=$next'> > </a>";
 			}
 
 			?>
