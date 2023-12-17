19fe6d85b24468e9402be2886e8c2b0f36122da3
evechen1226 <eveliving2021@gmail.com>
2023-12-17 19:12:42 +0800

    fix:modal/submenu.php-完成按下「更多次選單」新增項目列功能

---
+++ api/submenu.php
@@ -0,0 +1,20 @@
+<?php include_once "db.php";
+
+if(isset($_POST['id'])){
+  foreach($_POST['id'] as $idx => $id){
+
+
+
+  }
+}
+
+if(isset($_POST['add_text'])){
+  foreach($_POST['add_text'] as $idx => $text){
+if(isset($_POST['del'])&& in_array($id,$))
+
+  }
+}
+
+
+to("../back.php?do=menu");
+?>
