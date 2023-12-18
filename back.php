2d1ce4799913579df6fafc95d11a5b4979142e85
evechen1226 <eveliving2021@gmail.com>
2023-12-18 14:06:48 +0800

    feat:api/logout.php-新增登出功能

--- back.php
+++ back.php
@@ -1,4 +1,4 @@
-﻿<?php include_once "./api/db.php";?>
+﻿<?php include_once "./api/db.php"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0068)?do=admin&redo=title -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -25,14 +25,14 @@
 	<!-- <iframe style="display:none;" name="back" id="back"></iframe> -->
 	<div id="main">
 
-<!-- 顯示標題圖片 php -->
-<?php
-$title=$Title->find(['sh'=>1]);
-?>
-<!-- 顯示標題圖片 php END -->
+		<!-- 顯示標題圖片 php -->
+		<?php
+		$title = $Title->find(['sh' => 1]);
+		?>
+		<!-- 顯示標題圖片 php END -->
 
-		<a title="<?=$title['text'];?>" href="index.php">
-			<div class="ti" style="background:url('./img/<?=$title['img'];?>'); background-size:cover;"></div><!--標題-->
+		<a title="<?= $title['text']; ?>" href="index.php">
+			<div class="ti" style="background:url('./img/<?= $title['img']; ?>'); background-size:cover;"></div><!--標題-->
 		</a>
 		<div id="ms">
 			<div id="lf" style="float:left;">
@@ -79,8 +79,8 @@ $title=$Title->find(['sh'=>1]);
 
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-					<span class="t">進站總人數 : <?=$Total->find(1)['total'];?></span>
-					
+					<span class="t">進站總人數 : <?= $Total->find(1)['total']; ?></span>
+
 				</div>
 			</div>
 			<div class="di" style="height:540px; border:#999 1px solid; width:76.5%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
@@ -90,22 +90,22 @@ $title=$Title->find(['sh'=>1]);
 						<tr>
 							<td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;" class="cent"><a href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a>
 							</td>
-							<td><button onclick="document.cookie='user=';location.replace('?')" style="width:99%; margin-right:2px; height:50px;">管理登出</button></td>
+							<td><button onclick="location.href='./api/logout.php'" style="width:99%; margin-right:2px; height:50px;">管理登出</button></td>
 						</tr>
 					</tbody>
 				</table>
-<!-- back/title.php -->
-<?php 
-$do = $_GET['do'] ?? 'title';  
-$file = "./back/{$do}.php";
-if (file_exists($file)) {
-	include $file;
-} else {
-	include "./back/title.php";
-}
-?>
+				<!-- back/title.php -->
+				<?php
+				$do = $_GET['do'] ?? 'title';
+				$file = "./back/{$do}.php";
+				if (file_exists($file)) {
+					include $file;
+				} else {
+					include "./back/title.php";
+				}
+				?>
 
-<!-- back/title.php end-->
+				<!-- back/title.php end-->
 			</div>
 			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
 			</div>
@@ -127,7 +127,7 @@ if (file_exists($file)) {
 		</div>
 		<div style="clear:both;"></div>
 		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-			<span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
+			<span class="t" style="line-height:123px;"><?= $Bottom->find(1)['bottom']; ?></span>
 		</div>
 	</div>
 
