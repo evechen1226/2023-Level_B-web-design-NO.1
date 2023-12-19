aba82bc01fb21b629f6674dc51363f52ccd6351e
evechen1226 <eveliving2021@gmail.com>
2023-12-19 20:59:00 +0800

    fix:api/add.php-修正存圖片位置../img/，少一/，導致存取位置錯誤，存到根目錄

--- api/add.php
+++ api/add.php
@@ -1,6 +1,8 @@
 <?php 
 include_once "db.php";
 
+
+
 $DB=${ucfirst($_POST['table'])};
 //假設為 do=?img
 //$DB = Img
@@ -12,10 +14,10 @@ switch($table){
     break;
 }
 
+
 if(isset($_FILES['img']['tmp_name'])){
-    move_uploaded_file($_FILES['img']['tmp_name'],"../img".$_FILES['img']['name']);
+    move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
     $_POST['img']=$_FILES['img']['name'];
-    dd($_POST['img']);
 }
 
 if($table != 'admin'){
@@ -29,6 +31,6 @@ unset($_POST['table']);
 
 $DB->save($_POST);
 
-// to("../back.php?do=$table");
+to("../back.php?do=$table");
 
 ?>
