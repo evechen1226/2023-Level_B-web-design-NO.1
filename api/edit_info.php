32fddd382989dfe3302d4a2b89593b023601c14a
evechen1226 <eveliving2021@gmail.com>
2023-12-19 21:25:20 +0800

    fix:back/bottom.php-新增bottom.php頁面，修正api/edit_info.php-打錯['bottom']

--- api/edit_info.php
+++ api/edit_info.php
@@ -3,20 +3,21 @@
 //因為要共用，所以需要增加一input隱藏欄位存入當前網頁名稱
 
 //取得資料表名稱
-$table=$_POST['table'];
+$table=$_POST['table'];//$table=bottom
 
 //將資料表名稱轉成首字大寫的資料物件變數
-$DB=${ucfirst($table)};
+$DB=${ucfirst($table)};//$DB=$Bottom
 //$DB=$Table
 
 //取得id為1的資料
-$data-$DB->find(1);
+$data=$DB->find(1);
+
 
 //將資料中對應的欄位修改為POST過來值
-$data['$table']=$_POST['$table'];
+$data['bottom']=$_POST['bottom'];
 
 //使用save更新至資料表
-$DB->save($date);
+$DB->save($data);
 
 
 // 1.
---
+++ back/bottom.php
@@ -0,0 +1,36 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+	<p class="t cent botli">頁尾版權資料管理</p>
+	<!-- <form method="post" target="back" action="?do=tii"> -->
+	<form method="post" action="../api/edit_info.php">
+		<table style="width:50%;margin:auto">
+			<tbody>
+				<tr class="yel">
+					<td width="50%">頁尾版權資料管理</td>
+					<td width="50%">
+
+						<!-- <input type="number" name="total" id="" value="<? //=$Total->find(1)['total'];
+																																?>"></td> -->
+
+						<!-- 因為要共用，所以需要增加一input隱藏欄位存入當前網頁名稱 -->
+						<input type="text" name="bottom" value="<?= $Bottom->find(1)['bottom']; ?>">
+						<!-- <input type="hidden" name="table" value="bottom">  -->
+						<!-- 網址列有?do=bottom -->
+						<input type="hidden" name="table" value="<?= $do; ?>">
+
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+		<table style="margin-top:40px; width:100%;">
+			<tbody>
+				<tr>
+					<td class="cent">
+						<input type="submit" value="修改確定">
+						<input type="reset" value="重置">
+					</td>
+				</tr>
+			</tbody>
+		</table>
+
+	</form>
+</div>
