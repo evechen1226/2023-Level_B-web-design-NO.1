5f0273a3bdcde4082672edc194201496f7390253
evechen1226 <eveliving2021@gmail.com>
2024-01-12 08:25:46 +0800

    feat:db99.sql--更新資料庫db99

--- db99.sql
+++ db99.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-12-11 09:17:29
+-- 產生時間： 2024-01-12 01:19:39
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -42,10 +42,8 @@ INSERT INTO `ad` (`id`, `text`, `sh`) VALUES
 (2, '546546132', 0),
 (9, '12111253', 0),
 (11, '1232354', 0),
-(12, '12111253', 1),
-(13, '0', 1),
-(14, '0', 1),
-(15, 'aaaaaaaa', 1);
+(12, '12111253', 0),
+(15, 'aaaaaaaa', 0);
 
 -- --------------------------------------------------------
 
@@ -105,7 +103,12 @@ INSERT INTO `image` (`id`, `img`, `sh`) VALUES
 (1, '01C05.gif', 1),
 (3, '01D06.jpg', 1),
 (4, '01D01.jpg', 1),
-(5, '01D04.jpg', 0);
+(5, '01D04.jpg', 0),
+(6, '01D06.jpg', 1),
+(7, '01C05.gif', 1),
+(8, '01C06.gif', 1),
+(9, '01D03.jpg', 1),
+(10, '01D10.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -126,9 +129,9 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`id`, `text`, `href`, `sh`, `menu_id`) VALUES
-(12, '管理登入', 'index.php', 1, 0),
-(13, '網站首頁', 'index.php?do=login', 1, 0),
-(14, 'aaaaaaaa', 'adfsad', 0, 0);
+(13, '管理登入', '?do=login', 1, 0),
+(16, '網站首頁', 'index.php', 1, 0),
+(20, '更多內容', '?do=login', 1, 13);
 
 -- --------------------------------------------------------
 
@@ -147,10 +150,9 @@ CREATE TABLE `mvim` (
 --
 
 INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES
-(1, '01C04.gif', 0),
+(1, '01C04.gif', 1),
 (2, '01C01.gif', 0),
-(3, '01C02.gif', 0),
-(4, '01C03.gif', 0);
+(3, '01C02.gif', 0);
 
 -- --------------------------------------------------------
 
@@ -169,10 +171,14 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id`, `text`, `sh`) VALUES
-(1, '102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/', 0),
-(4, '', 0),
-(5, '102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/', 1),
-(6, '102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/', 1);
+(1, '教師研習「世界公民生命園丁國內研習會」\r\n1.主辦單位：世界展望會\r\n2.研習日期：101年11月14日（三）～15日（四）\r\n3.詳情請參考：\r\nhttp://gc.worldvision.org.tw/seed.html。\r\n請線上報名。\r\n', 0),
+(4, '公告綜合高中一年級英數補救教學時間\r\n上課日期:10/27.11/3.11/10.11/24共計四次\r\n上課時間:早上8:00~11:50半天\r\n費用:全程免費\r\n參加同學:綜合科一年級第一次段考成績需加強者\r\n已將名單送交各班及導師\r\n參加同學請帶紙筆.課本.第一次段考考卷\r\n並將家長通知單給家長\r\n若有任何疑問\r\n請洽綜合高中學程主任', 0),
+(5, '102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/\r\n', 1),
+(6, '台灣亞洲藝術文化教育交流學會第一屆年會國際研討會\r\n活動日期：101年3月3～4日(六、日)\r\n活動主題：創造力、文化、全人教育\r\n有意參加者請至http://www.caaetaiwan.org下載報名表', 1),
+(7, '11月23日(星期五)將於彰化縣田尾鄉菁芳園休閒農場\r\n舉辦「高中職生涯輔導知能研習」\r\n中區學校每校至多2名\r\n以普通科、專業類科教師優先報名參加\r\n生涯規劃教師次之，參加人員公差假\r\n並核實派代課\r\n當天還有專車接送(8:35前在員林火車站集合)\r\n如此好康的機會，怎能錯過？！\r\n熱烈邀請師長們向輔導室(分機234)報名\r\n名額有限，動作要快！！\r\n報名截止日期：本周四 10月25日17:00前！', 1),
+(8, '台視百萬大明星節目辦理海選活動\r\n時間:101年10月27日下午13時\r\n地點:彰化\r\n', 1),
+(9, '國立故宮博物院辦理\r\n「商王武丁與后婦好 殷商盛世文化藝術特展」暨\r\n「赫赫宗周 西周文化特展」\r\n', 1),
+(10, '財團法人漢光教育基金會\r\n辦理2012「舊愛新歡-古典詩詞譜曲創作暨歌唱表演競賽」\r\n參賽獎金豐厚!!\r\n', 1);
 
 -- --------------------------------------------------------
 
@@ -192,9 +198,9 @@ CREATE TABLE `titles` (
 --
 
 INSERT INTO `titles` (`id`, `img`, `text`, `sh`) VALUES
-(1, '01B01.jpg', '12111253', 1),
 (2, '01B03.jpg', 'agdfgasdfg', 0),
-(3, '01B04.jpg', '12111253', 0);
+(3, '01B04.jpg', '12111253', 1),
+(4, '01B01.jpg', '12111253', 0);
 
 -- --------------------------------------------------------
 
@@ -212,7 +218,7 @@ CREATE TABLE `total` (
 --
 
 INSERT INTO `total` (`id`, `total`) VALUES
-(1, 6);
+(1, 73);
 
 --
 -- 已傾印資料表的索引
@@ -298,13 +304,13 @@ ALTER TABLE `bottom`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `image`
 --
 ALTER TABLE `image`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `mvim`
@@ -316,13 +322,13 @@ ALTER TABLE `mvim`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `titles`
 --
 ALTER TABLE `titles`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `total`
