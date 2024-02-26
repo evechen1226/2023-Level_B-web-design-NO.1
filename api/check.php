ffbfbbfba75864210ddbc111c373d281628e3cdb
evechen1226 <eveliving2021@gmail.com>
2024-02-26 10:01:38 +0800

    註冊時增加md5編號，所以檢查時也要增加編碼才可相符

--- api/check.php
+++ api/check.php
@@ -13,7 +13,15 @@ include_once "db.php";
 // }
 
 // 在db.php增加資料過濾的函式 check()
-if($Admin->count(['acc'=>$_POST['acc'],'pw'=>$_POST['pw']])>0){
+// if($Admin->count(['acc'=>$_POST['acc'],'pw'=>$_POST['pw']])>0){
+//     $_SESSION['login']=$_POST['acc'];
+//     to("../back.php");
+// }else{
+//     to("../index.php?do=login&error=帳號密碼錯誤");
+// }
+
+// 使用 md5編碼, 之前使用明碼的資料無法登入
+if($Admin->count(['acc'=>$_POST['acc'],'pw'=>md5($_POST['pw'])])>0){
     $_SESSION['login']=$_POST['acc'];
     to("../back.php");
 }else{
